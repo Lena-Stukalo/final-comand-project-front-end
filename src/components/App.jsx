e85f511b39338c4ce4b633b43013fe4207dde54e
Yana Peshko <yanapeshko917@gmail.com>
2022-12-04 01:10:01 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-import CategoriesDropbox from './ModalAddTransaction/CategoriesDropbox/CategoriesDropbox';
-
 export const App = () => {
   return (
     <div
@@ -13,7 +11,6 @@ export const App = () => {
       }}
     >
       React homework template
-      <CategoriesDropbox />
     </div>
   );
 };
