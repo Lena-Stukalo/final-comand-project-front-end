1457022d4cce8d50aa46df604ed7aa89f5fadaeb
Oleksii Horban <lehagorban93@gmail.com>
2022-12-07 10:48:56 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 
 import Spinner from "./Spinner/Spinner";
-import Statistics from "./Statistics/Statistics";
+// import Statistics from "./Statistics/Statistics";
 // import { List } from "redux/data/data";
 import { useSelector } from "react-redux";
 import RegisterContainer from "./RegisterContainer";
 import Header from './Header';
+import Statistics from "./Statistics2/Statistics";
 
 export const App = () => {
 
