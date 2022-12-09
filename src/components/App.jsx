dbdf69d2b4a2071def10cceaf7d0ad52f5d9cccc
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-12-09 15:29:31 +0200

    add favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,22 @@
-
-import Spinner from "./Spinner/Spinner";
-import { List } from "redux/data/data";
-import { useSelector } from "react-redux";
-import RegisterContainer from "./RegisterContainer";
+import Spinner from './Spinner/Spinner';
+import { List } from 'redux/data/data';
+import { useSelector } from 'react-redux';
+import RegisterContainer from './RegisterContainer';
 import Header from './Header';
-import { Balance } from "./Balance/Balance";
-import { ModalLogout } from "./ModalLogout/ModalLogout";
+import { Balance } from './Balance/Balance';
+import { ModalLogout } from './ModalLogout/ModalLogout';
 
 export const App = () => {
+  const loading = useSelector(state => state.global.isLoading);
 
-
-   const loading = useSelector((state) => state.global.isLoading)
- 
   return (
-    
     <>
       {loading && <Spinner />}
-      <List/>   
-      <RegisterContainer/>
+      <List />
+      <RegisterContainer />
       <Header />
-      <Balance />
-      <ModalLogout />
+      {/* <Balance />
+        <ModalLogout /> */}
     </>
   );
 };
