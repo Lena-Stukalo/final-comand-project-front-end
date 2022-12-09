fe2250eda244cda0acbe6667420210c4986ca0f7
Oleksii Horban <lehagorban93@gmail.com>
2022-12-09 22:05:50 +0200

    calendar fix

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -5,21 +5,42 @@ import "react-datetime/css/react-datetime.css"
 import { useState } from "react";
 
 
-const Calendar = () => {
+
+const Calendar = () => {   
+   
 
     const [month, setMonth] = useState(false);
     const [year, setYear] = useState(false);
+    const [openCalendar, setOpenCalendar] = useState(true)
 
-    const toggleMonth = (e) => {        
+    const toggleMonth = () => {        
         setMonth(!month)
         setYear(false)
+       
     }
 
     const toggleYear = () => {
         setYear(!year)
         setMonth(false)
     }
-
+     
+    const toggleCalendar = (e) => {
+        
+        const classTable = e.target.getAttribute("class")
+        
+        
+        if (classTable === "rdtMonth") {
+             setOpenCalendar(false)
+        }
+        else if (classTable === "rdtYear") {
+             setOpenCalendar(false)
+        }
+        else {
+            setOpenCalendar(true)
+        }
+        
+        
+    } 
    
 
     
@@ -27,7 +48,7 @@ const Calendar = () => {
 
     return (      
         
-             <div className={style.wrappCalendar}>
+             <div onClick={toggleCalendar} className={style.wrappCalendar}>
             <div>
                             <div onClick={toggleMonth} className={style.wrapperMonth}>
                            
@@ -35,7 +56,7 @@ const Calendar = () => {
                 <img src={arrow} alt=">" />
                 
                 </div>
-                {month && <Datetime timeFormat={false}  open={true} className={style.datetime} />}
+                {month && <Datetime open={openCalendar} closeOnSelect={true} timeFormat={false} dateFormat="MM"   className={style.datetime} />}
             </div>
             
             <div>
@@ -43,8 +64,9 @@ const Calendar = () => {
             <p className={style.calendarText}>Year</p>
             <img src={arrow} alt=">" />
             </div>  
-            {year && <Datetime className={style.datetimeYear} dateFormat="YYYY" timeFormat={false} open={true} />}
-        </div>
+            {year && <Datetime open={openCalendar}  closeOnSelect={true}  className={style.datetimeYear} dateFormat="YYYY" timeFormat={false}  />}
+            </div>
+            
             </div> 
             
         
