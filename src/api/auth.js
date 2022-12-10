26e59f4f3d9fad536ecdf1d60b5820bb695b1931
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-12-10 17:56:59 +0200

    sinup update

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,16 +1,3 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:5000',
-});
-
-const setToken = token => {
-  if (token) {
-    return (instance.defaults.headers.common.authorization = `Bearer ${token}`);
-  }
-  instance.defaults.headers.common.authorization = '';
-};
-
 export const signup = async data => {
   const requestData = {
     name: data.firstName,
