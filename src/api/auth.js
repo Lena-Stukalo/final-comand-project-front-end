dddbe50f4cf4ad77411040c42ef7a38b36ad601d
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-12-10 17:41:22 +0200

    registration redux clear

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,31 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000"
+  baseURL: 'http://localhost:5000',
 });
 
-const setToken = (token) => {
-    if (token) {
-        return instance.defaults.headers.common.authorization = `Bearer ${token}`;
-    }
-    instance.defaults.headers.common.authorization = '';
-}
+const setToken = token => {
+  if (token) {
+    return (instance.defaults.headers.common.authorization = `Bearer ${token}`);
+  }
+  instance.defaults.headers.common.authorization = '';
+};
 
-export const signup = async (data) => {
-    const requestData = {
-        name: data.firstName,
-        password: data.password,
-        email: data.email
-    };
+export const signup = async data => {
+  const requestData = {
+    name: data.firstName,
+    password: data.password,
+    email: data.email,
+  };
 
-    const { data: result } = await instance.post("/api/auth/register", requestData);
-
-    if (!result.token) {
-        console.log("TOKEN IS UNDEFINED, SET MANUALLY FOR DEBUG")
-        result.token = 'xxx';
-    }
-    setToken(result.token);
-    return result;
-}
-
-
+  return requestData;
+};
--- src/redux/auth/auth-selector.js
+++
@@ -1,6 +0,0 @@
-export const getUser = state => state.auth.user;
-export const getIsLoggedIn = state => state.auth.isLoggedIn;
-export const getIsRefreshing = state => state.auth.isRefreshing;
-export const getToken = state => state.auth.token;
-export const getError = state => state.auth.error;
-export const getUserBalance = state => state.auth.user.balance;
