ebbffd823a146460d5ca16ccb4730cb2f3fe78dd
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-12-12 00:01:13 +0200

    change app name + visual update

--- src/pages/StatisticPage/StatisticPage.jsx
+++ src/pages/StatisticPage/StatisticPage.jsx
@@ -2,11 +2,12 @@ import StatisticContainer from 'components/StatisticContainer/StatisticContainer
 import css from './StatisticPage.module.css';
 import { useSelector } from 'react-redux';
 import AuthSelectors from 'redux/auth/authSelectors';
+import Spinner from 'components/Spinner/Spinner';
 
 export const StatisticPage = () => {
   const isRefreshing = useSelector(AuthSelectors.getIsRefreshing);
   return isRefreshing ? (
-    <b>Refreshing data...</b>
+    <Spinner />
   ) : (
     <div>
       <h2 className={css.statisticTitle}>Statistics</h2>
