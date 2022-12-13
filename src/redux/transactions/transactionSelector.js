b6efe6007e803dcc1796c1488971c634cf876054
SashaDyts <lentrofan@gmail.com>
2022-12-13 20:14:22 +0200

    fix

--- src/redux/transactions/transactionSelector.js
+++ src/redux/transactions/transactionSelector.js
@@ -5,22 +5,34 @@ const getError = state => state.transactions.error;
 const getDetailsIncome = state => state.transactions.details.totalIncome;
 const getDetailsExpense = state => state.transactions.details.totalExpense;
 const getTransactions = state => state.transactions.data;
+// const getFilteredTransactions = state => {
+//   const regExNoDots = /[^\d]/g;
+
+//   const dataToSotr = [...state.transactions.data];
+
+//   return dataToSotr.sort(
+//     (firstEl, secondEl) =>
+//       Number(
+//         secondEl.date.replace(regExNoDots, '') +
+//           secondEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '')
+//       ) -
+//       Number(
+//         firstEl.date.replace(regExNoDots, '') +
+//           firstEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '')
+//       )
+//   );
+// };
 const getFilteredTransactions = state => {
   const regExNoDots = /[^\d]/g;
+  const data = [...state.transactions.data];
 
-  const dataToSotr = [...state.transactions.data];
-
-  return dataToSotr.sort(
+  const result = data.sort(
     (firstEl, secondEl) =>
-      Number(
-        secondEl.date.replace(regExNoDots, '') +
-          secondEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '')
-      ) -
-      Number(
-        firstEl.date.replace(regExNoDots, '') +
-          firstEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '')
-      )
+      secondEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '') -
+      firstEl.createdAt.slice(11).substring(0, 8).replace(regExNoDots, '')
   );
+
+  return result;
 };
 const TransSelectors = {
   getIsLoading,
